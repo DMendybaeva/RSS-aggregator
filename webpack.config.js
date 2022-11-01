7faf5dc8da78491dae46de40678d781777f5e7c0
Diana Mendybaeva <DRMendybaeva@gmail.com>
2022-11-01 22:57:17 +0500

    configure webpack with bootstrap

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default () => ({
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(path.resolve(), './dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'index.html' }),
+  ],
+  performance: {
+    hints: false,
+  },
+});
